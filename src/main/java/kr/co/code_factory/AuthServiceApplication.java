49162d563f2091083a07999bd24795633e158bc0
yonggyo1125 <yonggyo00@kakao.com>
2025-09-22 12:35:22 +0900

    .

---
+++ src/main/java/kr/co/code_factory/AuthServiceApplication.java
@@ -0,0 +1,15 @@
+package kr.co.code_factory;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class AuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/code_factory/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.code_factory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
